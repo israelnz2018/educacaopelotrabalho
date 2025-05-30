ead10c3912caf0a015d0540c115cfd4e2f3a67e9
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-05-31 10:16:25 +1200

    suporte.py

--- suporte.py
+++ suporte.py
@@ -4,9 +4,12 @@ def interpretar_coluna(df, nome_coluna):
     """
     try:
         nome_coluna = nome_coluna.strip().upper()
+        if len(nome_coluna) != 1 or not nome_coluna.isalpha():
+            raise ValueError
         index = ord(nome_coluna) - ord('A')
         return df.columns[index]
     except Exception:
         raise ValueError(f"Coluna '{nome_coluna}' é inválida.")
+
 
 
