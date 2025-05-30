a2840dddc3bb8e544888de7919f4867fc3b0533b
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-05-31 10:04:14 +1200

    suporte.py

--- suporte.py
+++ suporte.py
@@ -3,8 +3,10 @@ def interpretar_coluna(df, nome_coluna):
     Interpreta a coluna informada (como 'A', 'B', 'C') e retorna o nome real da coluna no DataFrame.
     """
     try:
-        index = ord(nome_coluna.upper()) - ord('A')
+        nome_coluna = nome_coluna.strip().upper()
+        index = ord(nome_coluna) - ord('A')
         return df.columns[index]
     except Exception:
         raise ValueError(f"Coluna '{nome_coluna}' é inválida.")
+
 
