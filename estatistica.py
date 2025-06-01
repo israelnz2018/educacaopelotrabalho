6fdd14ffd8c11dc530e22486ce70fce6f1fed9cd
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-01 16:48:43 +1200

    estatistica.py

--- estatistica.py
+++ estatistica.py
@@ -426,6 +426,7 @@ ANALISES = {
     "teste_normalidade": teste_normalidade,
     "regressao_logistica_binaria": analise_regressao_logistica_binaria,
     "regressao_logistica_nominal": analise_regressao_logistica_nominal,
-    "regressao_logistica_ordinal": analise_regressao_logistica_ordinal,
+    "regressao_logistica_ordinal": analise_regressao_logistica_ordinal
+    
 }
 
